9deb5fc41df70b387cc17496f7bca5aa41520625
Kris Young <ckx.ict@gmail.com>
2024-06-12 02:12:03 +0800

    add sample

---
+++ 494.目标和.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ *
+ * [494] 目标和
+ */
+
+#inlcude<vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+    }
+};
+// @lc code=end
+

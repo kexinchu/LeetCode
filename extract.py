5e426f6a48e041728adc4ef80b7364b5fe79d098
Kris Young <ckx.ict@gmail.com>
2024-06-14 19:58:33 +0800

    hyDNS

--- extract.py
+++ extract.py
@@ -3,6 +3,13 @@ import random
 import sys
 
 if __name__ == "__main__":
+    """
+    入参：
+        1, 文件路径
+        2, cache mis比例
+
+    set会产出乱序,取乱序后的 len * 0.2 : len 个元素即可
+    """
     url_set = set()
     with open(sys.argv[1], "r") as fr:
         for line in fr:
